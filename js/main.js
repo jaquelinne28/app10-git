3ca0406795b4c5d9c848ebd7e2d1e75687111f26
jaquelinne28 <lealjaquie02@gmail.com>
2019-05-29 09:55:28 -0500

    3

--- js/main.js
+++ js/main.js
@@ -7,16 +7,16 @@ $('#pause').hide();
 initAudio($('#playlist li:first-child'));
 
 function initAudio(element){
-	var song=element.attr('song');
-	var title=elemnt.text();
-	var cover=element.attr('cover');
-	var artist=element.attr('artist');
+	var song = element.attr('song');
+	var title = element.text();
+	var cover = element.attr('cover');
+	var artist = element.attr('artist');
 	
 	//Create a New Audio Object
-	audio=new Audio('media/' + song);
+	audio = new Audio('media/'+ song);
 	
 	if(!audio.currentTime){
-		$('#duracion').html('0.00');
+		$('#duration').html('0.00');
 	}
 	
 	$('#audio-player .title').text(title);
@@ -25,93 +25,91 @@ function initAudio(element){
 	//Insert Cover Image
 	$('img.cover').attr('src','images/covers/' + cover);
 	
-	$('playlist li').removeClass('active');
+	$('#playlist li').removeClass('active');
 	element.addClass('active');
 }
 
 
 //Play Button
-$('#play').click(function(){
-	audio.play();
+$('#play').click(function (){
+	audio.play ();
 	$('#play').hide();
 	$('#pause').show();
-	$('#duration').fadeln(400);
+	$('#duration').fadeIn(400);
 	showDuration();
 });
 
 //Pause Button
-$('#pause').click(function(){
-	audio.pause();
+$('#pause').click(function (){
+	audio.pause ();
 	$('#pause').hide();
 	$('#play').show();
 });
 
 //Stop Button
-$('#stop').click(function(){
-	audio.pause();
-	audio.currentTime=0;
-	$('#pause').hidde();
+$('#stop').click(function (){
+	audio.pause ();
+	audio.currentTime =0;
+	$('#pause').hide();
 	$('#play').show();
-	$('duration').fadeOut(400);
+	$('#duration').fadeOut(400);
 });
 
 //Next Button
-$('#next').click(function(){
+$('#next').click(function (){
 	audio.pause ();
 	var next=$('#playlist li.active').next();
-	if (next.length==0){
-	 next=$('#playlist li:fir-child');	
-}
-initAudio (next);
-audio.play();
-showDuration();
+	if(next.length==0){
+		next=$('#playlist li:first-child');
+	}
+	initAudio (next);
+	audio.play();
+	showDuration();
 });
 
-//Prev Button
+//PrevButton
 $('#prev').click(function (){
 	audio.pause();
-	var prev=$('#playlist li.active').prev();
-	if(prev.length==0){
-		prev=$('#playlist li:last-child');
+	var prev=$('#palylist li.active').prev();
+	if(prev.length ==0){
+		prev =$('#playlist li:last-child');
 	}
 	initAudio (prev);
-	audio.play();
+	audio.paly();
 	showDuration();
 });
 
 //Playlist Song Click
-$('#playlist li').click(function(){
+$('#playlist li').click(function () {
 	audio.pause();
 	initAudio($(this));
 	$('#play').hide();
 	$('#pause').show();
-	$('#duration').fadeln(400);
+	$('#duration').fadeIn(400);
 	audio.play();
 	showDuration();
 });
 
-//Volume Control
-$('#volume').change(function (){
-	audio.volume= parseFloat (this.value/10);
+//Volumen Control
+$('#volumen').change(function (){
+	audio.volume =parseFloat(this.value/10);
 });
 
-//Time Duration(){
-	function showDuration(){
+//Time Duration
+function showDuration (){
 	$(audio).bind('timeupdate', function(){
 		//Get hours and minutes
-		var s= parseInt(audio.currentTime % 60);
-		var m= parseInt((audio.currentTime /60)%60);
-		//Add 0 if seconds less than 10
+		var s = parseInt(audio.currentTime % 60);
+		var m = parseInt((audio.currentTime/60)% 60);
+		//Add 0 if seconds less than 100
 		if(s<10){
 			s='0' + s;
 		}
-		$('#duration').html(m +'.'+s);
+		$('#duration').html(m + '.' + s);
 		var value=0;
 		if(audio.currentTime>0){
-			value =Math.floor((100/audio.duration)*audio.currentTime);
+			value = Math.floor((100/audio.duration)*audio.currentTime);
 		}
 		$('#progress').css('width',value+'%');
 	});
 }
-	
-	
